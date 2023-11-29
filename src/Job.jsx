4a71ee7ea79fc305a6fed8e6c1c3975e0192ad62
Raj Kiran Chaudhary <raj.chaudhary@ebpearls.com>
2023-11-29 16:16:24 +0545

    value based on index added

--- src/Job.jsx
+++ src/Job.jsx
@@ -6,34 +6,39 @@ const url = "https://course-api.com/react-tabs-project";
 export const Job = () => {
   const [loading, setLoading] = useState(true);
   const [jobList, setJobList] = useState([]);
+  const [indexValue, setIndexValue] = useState(0);
 
   const fetchJobs = async () => {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    setJobList(data);
+    setLoading(false);
   };
 
   useEffect(() => {
     fetchJobs();
   }, []);
 
+  if (loading) {
+    return <h1>Loading ..</h1>;
+  }
+
+  const { title, dates, duties, company } = jobList[indexValue];
+  console.log(title);
+
   return (
     <div className="job-lists">
-      <h2>FullStack Web Dev</h2>
-      <button>Company name</button>
-      <p>December 2015 - Present</p>
-      <div className="list">
-        <div className="job">
-          <FiChevronsRight />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-            in ea, provident molestiae quaerat consequuntur repellendus eligendi
-            reprehenderit ullam modi? Natus, a! Labore sint, illo non natus
-            explicabo, laudantium veritatis, doloremque dolore voluptates et
-            officia!
-          </p>
-        </div>
-      </div>
+      <h2>{title}</h2>
+      <button>{company}</button>
+      <h3>{dates}</h3>
+      {duties.map((duty) => {
+        return (
+          <div className="job">
+            <FiChevronsRight />
+            <h4>{duty}</h4>
+          </div>
+        );
+      })}
     </div>
   );
 };
